6d5d4271a96c480ba5ccce2f9c8ea7dba0983458
Hugo Duran <hu.durannn@gmail.com>
2017-02-22 12:34:54 -0300

    third commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :category
+  belongs_to :user
+  
 end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+	has_many :items
+	validates :name, presence: true
+	validates :email, uniqueness: true
+	before_validation :normalize_name, on: :create
+
+	protected
+
+    def normalize_name
+      self.name = name.upcase.titleize
+    end
+end
